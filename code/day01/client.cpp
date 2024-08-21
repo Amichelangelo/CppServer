b1d781145d2c76bf183d5996fc734665be8a921f
Qi <dumuqiaotime@outlook.com>
2024-08-21 11:38:14 +0800

    some changes

--- code/day01/client.cpp
+++ code/day01/client.cpp
@@ -6,15 +6,24 @@
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \30dayMakeCppServer\code\day01\client.cpp
  */
-#include <sys/socket.h>
-#include <arpa/inet.h>
+
 #include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+// 链接 Winsock 库
+#pragma comment(lib, "ws2_32.lib")
 
 int main() {
+
+    WSADATA wsaData; // WSADATA is a struct defined in winsock2.h
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in serv_addr;
-    bzero(&serv_addr, sizeof(serv_addr));
+    sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8888);
@@ -23,5 +32,8 @@ int main() {
 
     connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));    
     
+    closesocket(sockfd);
+    WSACleanup();
+
     return 0;
 }
--- code/day01/server.cpp
+++ code/day01/server.cpp
@@ -1,27 +1,33 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")  // Link with ws2_32.lib
 
 int main() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2,2), &wsaData);
+
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in serv_addr;
-    bzero(&serv_addr, sizeof(serv_addr));
+    sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8888);
 
-    bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+    bind(sockfd, (SOCKADDR*)&serv_addr, sizeof(serv_addr));
 
     listen(sockfd, SOMAXCONN);
     
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_len = sizeof(clnt_addr);
-    bzero(&clnt_addr, sizeof(clnt_addr));
+    sockaddr_in clnt_addr;
+    int clnt_addr_len = sizeof(clnt_addr);
+    memset(&clnt_addr, 0, sizeof(clnt_addr));
 
-    int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
+    int clnt_sockfd = accept(sockfd, (SOCKADDR*)&clnt_addr, &clnt_addr_len);
 
     printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+    
+    WSACleanup();
     return 0;
 }
